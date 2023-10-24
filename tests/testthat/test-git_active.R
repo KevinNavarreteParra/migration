9f53e1e38b5be25528d415b55349e17f5a24b2b3
Kevin <navark4@unlv.nevada.edu>
2023-10-24 08:05:20 -0700

    split testthat into two different tests for git_active

--- tests/testthat/test-git_active.R
+++ tests/testthat/test-git_active.R
@@ -1,28 +1,31 @@
-# test_that("git_active function works as expected", {
-#
-#   orig_wd <- getwd()
-#
-#   # Test 1: When the git repository is active
-#   expect_true({
-#     tempdir <- tempdir()
-#     setwd(tempdir)
-#     system("git init")
-#     isActive <- git_active()
-#     setwd(tempdir)
-#     system("rm -rf .git")  # Remove the Git directory
-#     isActive
-#   })
-#
-#   # Test 2: When the git repository is not active
-#   expect_false({
-#     tempdir <- tempdir()
-#     setwd(tempdir)
-#     isActive <- git_active()
-#     setwd(tempdir)
-#     !isActive
-#   })
-#
-#   setwd(orig_wd)
-# })
+test_that("git_active function detects git = TRUE", {
 
-test_that("git_active function works as expected", {})
+  orig_wd <- getwd()
+
+  # Test 1: When the git repository is active
+  expect_true({
+    tempdir <- tempdir()
+    setwd(tempdir)
+    system("git init")
+    isActive <- git_active()
+    setwd(tempdir)
+    system("rm -rf .git")  # Remove the Git directory
+    isActive
+  })
+
+
+  setwd(orig_wd)
+})
+
+test_that("git_active function detects git = FALSE", {
+
+  # Test 2: When the git repository is not active
+  expect_false({
+    tempdir <- tempdir()
+    setwd(tempdir)
+    isActive <- git_active()
+    setwd(tempdir)
+    !isActive
+  })
+
+})
