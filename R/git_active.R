b56426d4ee3c05f9aa329061cc4081e591d556c9
Kevin <navark4@unlv.nevada.edu>
2023-10-23 22:11:32 -0700

    git_active skeleton

--- R/git_active.R
+++ R/git_active.R
@@ -1,8 +1,17 @@
+#' Determine whether git is active in active project
+#'
+#' @return Logical value, whether git is installed in active project
+#' @export
 git_active <- function() {
-  result <- system("git rev-parse --is-inside-work-tree", intern = TRUE)
-  if (length(result) > 0 && result[1] == "true") {
-    return(TRUE)
-  } else {
-    return(FALSE)
-  }
+result <- system("git rev-parse --is-inside-work-tree", intern = TRUE)
+if (length(result) > 0 && result[1] == "true") {
+  return(TRUE)
+} else {
+  return(FALSE)
 }
+}
+#'
+#' @examples
+#' git_active()
+#'
+
